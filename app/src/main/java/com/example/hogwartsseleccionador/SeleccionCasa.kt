ea138805eabc1226816e52d3594f67b68c8a157b
shen <santi.dionis@gmail.com>
2023-04-03 20:50:37 +0200

    ADD : Capa Seleccion Casa

---
+++ app/src/main/java/com/example/hogwartsseleccionador/SeleccionCasa.kt
@@ -0,0 +1,11 @@
+package com.example.hogwartsseleccionador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeleccionCasa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccion_casa)
+    }
+}
